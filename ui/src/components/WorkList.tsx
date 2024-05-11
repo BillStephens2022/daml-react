31ff74738872d591a46e3faf6117a8976dcf8c01
BillStephens2022 <two4onebill@yahoo.com>
2024-05-11 09:00:43 -0400

    added functionality in UI to reject a work proposal and provide feedback

--- ui/src/components/WorkList.tsx
+++ ui/src/components/WorkList.tsx
@@ -9,11 +9,12 @@ import {
   Grid,
   Button,
   Icon,
+  Modal,
 } from "semantic-ui-react";
 import { ContractId, Party } from "@daml/types";
 import { Ledger, CreateEvent } from "@daml/ledger";
 import { Work } from "@daml.js/daml-react";
-
+import RejectForm from "./RejectForm";
 
 type Props = {
   partyToAlias: Map<Party, string>;
@@ -32,9 +33,14 @@ const WorkList: React.FC<Props> = ({
   username,
   isWorkerList,
   isWorkContract,
-  ledger
+  ledger,
 }) => {
+    // state for modal showing the Reject Proposal Form
   const [showRejectForm, setShowRejectForm] = useState(false);
+  // id of proposal selected
+  const [selectedProposalId, setSelectedProposalId] =
+    useState<ContractId<Work.WorkProposal> | null>(null);
+
   // Filter proposals where the current user is the client
   const clientProposals = workProposals.filter(
     (proposal) => proposal.payload.client === username
@@ -54,15 +60,15 @@ const WorkList: React.FC<Props> = ({
     "Pending Worker Approval",
   ];
 
-  const contractHeaders = [
-    "Accepted Work Contracts",
-    "Active Contracts"
-  ]
+  const contractHeaders = ["Accepted Work Contracts", "Active Contracts"];
 
   // Determine which headers, contracts to use based on isWorkContracct & isWorkerList
-  const headersToUse = isWorkContract ? contractHeaders : (isWorkerList ? workerHeaders : clientHeaders);
+  const headersToUse = isWorkContract
+    ? contractHeaders
+    : isWorkerList
+    ? workerHeaders
+    : clientHeaders;
   const contractsToUse = isWorkContract ? workContracts : workProposals;
-  
 
   const acceptProposal = async (contractId: ContractId<Work.WorkProposal>) => {
     try {
@@ -74,9 +80,30 @@ const WorkList: React.FC<Props> = ({
     }
   };
 
-  const openRejectForm = (contractId: ContractId<Work.WorkProposal>) => {
+  const openRejectForm = (
+    contractId: ContractId<Work.WorkProposal>
+  ) => {
+    setSelectedProposalId(contractId); // Set the selected proposal ID
     setShowRejectForm(true);
-  }
+  };
+
+  const handleRejectProposal = async (feedback: string) => {
+    console.log("Before try block in rejectPRoposal handler: ", feedback);
+    try {
+      if (selectedProposalId) {
+        await ledger.exercise(
+          Work.WorkProposal.RejectProposal,
+          selectedProposalId,
+          { feedback }
+        );
+        console.log("Proposal rejected:", selectedProposalId);
+        setShowRejectForm(false);
+        setSelectedProposalId(null);
+      }
+    } catch (error) {
+      console.error("Error rejecting proposal:", error);
+    }
+  };
 
   return (
     <Segment>
@@ -96,46 +123,102 @@ const WorkList: React.FC<Props> = ({
         {contractsToUse.map((contract) => (
           <Grid.Column key={contract.contractId}>
             <Segment style={{ minWidth: 0, width: "auto" }}>
-              <Header as="h3">{isWorkContract ? (contract.payload as Work.WorkContract).contractJobTitle : (contract.payload as Work.WorkProposal).jobTitle}</Header>
+              <Header as="h3">
+                {isWorkContract
+                  ? (contract.payload as Work.WorkContract).contractJobTitle
+                  : (contract.payload as Work.WorkProposal).jobTitle}
+              </Header>
               <p>
                 <strong>Client:</strong>{" "}
-                {partyToAlias.get(isWorkContract ? (contract.payload as Work.WorkContract).contractClient : (contract.payload as Work.WorkProposal).client) ?? "Unknown"}
+                {partyToAlias.get(
+                  isWorkContract
+                    ? (contract.payload as Work.WorkContract).contractClient
+                    : (contract.payload as Work.WorkProposal).client
+                ) ?? "Unknown"}
               </p>
               <p>
                 <strong>Worker:</strong>{" "}
-                {partyToAlias.get(isWorkContract ? (contract.payload as Work.WorkContract).contractWorker : (contract.payload as Work.WorkProposal).worker) ?? "Unknown"}
+                {partyToAlias.get(
+                  isWorkContract
+                    ? (contract.payload as Work.WorkContract).contractWorker
+                    : (contract.payload as Work.WorkProposal).worker
+                ) ?? "Unknown"}
               </p>
               <p>
-                <strong>Category:</strong> {isWorkContract ? (contract.payload as Work.WorkContract).contractJobCategory : (contract.payload as Work.WorkProposal).jobCategory}
+                <strong>Category:</strong>{" "}
+                {isWorkContract
+                  ? (contract.payload as Work.WorkContract).contractJobCategory
+                  : (contract.payload as Work.WorkProposal).jobCategory}
               </p>
               <p>
-                <strong>Description:</strong> {isWorkContract ? (contract.payload as Work.WorkContract).contractJobDescription : (contract.payload as Work.WorkProposal).jobDescription}
+                <strong>Description:</strong>{" "}
+                {isWorkContract
+                  ? (contract.payload as Work.WorkContract)
+                      .contractJobDescription
+                  : (contract.payload as Work.WorkProposal).jobDescription}
               </p>
               <p>
-                <strong>Note:</strong> {isWorkContract ? null : (contract.payload as Work.WorkProposal).note}
+                <strong>Note:</strong>{" "}
+                {isWorkContract
+                  ? null
+                  : (contract.payload as Work.WorkProposal).note}
               </p>
               <p>
-                <strong>Rate Type:</strong> {isWorkContract ? (contract.payload as Work.WorkContract).contractRateType : (contract.payload as Work.WorkProposal).rateType}
+                <strong>Rate Type:</strong>{" "}
+                {isWorkContract
+                  ? (contract.payload as Work.WorkContract).contractRateType
+                  : (contract.payload as Work.WorkProposal).rateType}
               </p>
               <p>
-                <strong>Rate Amount:</strong> {isWorkContract ? (contract.payload as Work.WorkContract).contractRateAmount : (contract.payload as Work.WorkProposal).rateAmount}
+                <strong>Rate Amount:</strong>{" "}
+                {isWorkContract
+                  ? (contract.payload as Work.WorkContract).contractRateAmount
+                  : (contract.payload as Work.WorkProposal).rateAmount}
               </p>
               {isWorkerList && (
                 <Button.Group fluid>
                   <Button
                     color="blue"
-                    onClick={() => acceptProposal((contract.contractId as ContractId<Work.WorkProposal>))}
+                    onClick={() =>
+                      acceptProposal(
+                        contract.contractId as ContractId<Work.WorkProposal>
+                      )
+                    }
                   >
                     Accept
                   </Button>
                   <Button.Or />
-                  <Button color="red" onClick={() => openRejectForm(contract.contractId as ContractId<Work.WorkProposal>)}>Reject</Button>
+                  <Button
+                    color="red"
+                    onClick={() =>
+                      openRejectForm(
+                        contract.contractId as ContractId<Work.WorkProposal>
+                      )
+                    }
+                  >
+                    Reject
+                  </Button>
                 </Button.Group>
               )}
             </Segment>
           </Grid.Column>
         ))}
       </Grid>
+      <Modal
+        open={showRejectForm}
+        onClose={() => {
+          setShowRejectForm(false);
+          setSelectedProposalId(null);
+        }}
+      >
+        <Modal.Header>Reject Proposal</Modal.Header>
+        <Modal.Content>
+          <RejectForm
+            onSubmit={(feedback) => handleRejectProposal(feedback)}
+            onCancel={() => setShowRejectForm(false)}
+          />
+        </Modal.Content>
+      </Modal>
     </Segment>
   );
 };
